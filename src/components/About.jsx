4df6229fe3db1ce3d73d905738272436927771f4
Muhammad Usaid Javaid <iUsaidJavaid@users.noreply.github.com>
2023-12-05 11:47:31 +0500

    123

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className={`w-1 sm:h-80 h-40 violet-gradient`} />
+        </div>
 
-export default About
+        <div className="mt-10 text-white">
+          <h2 className="text-xl font-bold">About Me</h2>
+          <p className="mt-2">
+            Mobile App Developer at Codegenio with a Bachelor's degree from ARID
+            University.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <section className="w-full mx-auto">
+      <div className="max-w-2xl mx-auto mt-10 text-center">
+        <h2 className="text-2xl font-bold">Contact Me</h2>
+        <div className="mt-8">
+          <p className="text-lg">
+            <span className="font-bold">Email:</span>
+            <br />
+            iusaidjavaid@gmail.com
+            <br />
+            usaid.javaid@codegenio.com
+          </p>
+          <p className="mt-4 text-lg">
+            <span className="font-bold">Location:</span>
+            <br />
+            Civic Center, Bahria Town Phase 4
+          </p>
+          <p className="mt-4 text-lg">
+            <span className="font-bold">Phone:</span>
+            <br />
+            03185400014
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
